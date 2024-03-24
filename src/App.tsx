8e6e06a63ef8e89ef43f444ca944a33cd0b8b685
SumittaBungsud <sumitta.hct@gmail.com>
2024-03-25 00:18:55 +0700

    upload new code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="bg-sky-500">
-      <h1 className="text-3xl font-bold underline text-neutral-500">
+    <div className="bg-sky-400 w-dvw h-dvh">
+      <h1 className="text-3xl font-bold text-neutral-200 text-center p-3">
         Hello world!
       </h1>
     </div>
