97d939115baea0cc4be0ce2850140636964c096f
SumittaBungsud <sumitta.hct@gmail.com>
2024-09-18 23:50:08 +0700

    Updated candy crush improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import ColorBoard from './components/ColorBoard';
+import CandyBoard from "./components/CandyBoard";
 
 function App() {
   return (
     <div className="flex flex-col justify-center bg-sky-200 w-dvw h-dvh">
       <div className="flex justify-center">
-        <h1 className="w-1/2 text-3xl text-center font-bold text-neutral-600 p-4 border-b-4 border-teal-500">Candy Crush Board</h1>
+        <h1 className="w-1/2 text-3xl text-center font-bold text-neutral-600 p-4 border-b-4 border-teal-500">
+          Candy Crush Board
+        </h1>
       </div>
-      <ColorBoard/>
+      <CandyBoard />
     </div>
   );
 }
